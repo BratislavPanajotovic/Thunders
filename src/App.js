93413d9d9190954fac0c0428b862d492ea8bd171
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-05-03 22:34:36 +0200

    useRef to persist data between renders

--- src/App.js
+++ src/App.js
@@ -163,7 +163,10 @@ function Search({ query, setQuery }) {
       }
     };
     document.addEventListener("keydown", callback);
-  }, []);
+    return () => {
+      document.addEventListener("keydown", callback);
+    };
+  }, [setQuery]);
   return (
     <input
       className="search"
@@ -219,6 +222,12 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
+
+  const countRef = useRef(0);
+  useEffect(() => {
+    if (userRating) countRef.current = countRef.current++;
+  }, [userRating]);
+
   const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
   const watchedUserrating = watched.find(
     (movie) => movie.imdbID === selectedId
@@ -245,6 +254,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
       runtime: Number(runtime.split(" ").at(0)),
       userRating,
+      countRatingDecisions: countRef.current,
     };
 
     onAddWatched(newWatchedMovie);
@@ -315,6 +325,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
                     maxRating={10}
                     size={27.5}
                     onSetRating={setUserRating}
+                    ref={countRef}
                   />{" "}
                   {userRating > 0 && (
                     <button className="btn-add" onClick={handleAdd}>
